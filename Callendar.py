9fd268c595a63966742e4cbc8b6d8d452b855b71
Jch03 <ken4098@naver.com>
2025-06-19 17:37:00 +0900

    delete_schedule

--- Callendar.py
+++ Callendar.py
@@ -41,6 +41,7 @@ def load_file(filename):
     with open(filepath, 'r', encoding='utf-8') as f:
         return json.load(f)
 
+#일정 추가
 def add_schedule():
     content = input("일정 내용을 입력하세요 :")
     uid = generate_id()
@@ -49,6 +50,134 @@ def add_schedule():
     print(f"일정이 저장되었습니다: {filename}")
     
 
+#일정 조회
+def check_schedule():
+    directory = "schedules"
+    if not os.path.exists(directory):
+        print("오류 : 일정이 존재하지 않습니다.")
+        return
+    
+    files = os.listdir(directory)
+    if not files:
+        print("오류 : 일정이 존재하지 않습니다.")
+        return
+    
+    print ("조회할 일정 단위를 선택하세요.")
+    print("1. 년도별 2. 월별 3. 일별")
+    ch = input("")
+    year = month = day = None
+    
+    if ch == "1":
+        year = input("조회할 년도를 입력해주세요.")
+        
+    elif ch == "2":
+        year = input("조회할 년도를 입력해주세요.")
+        month = input("해당 년도의 조회할 월을 입력해주세요.")
+        
+    elif ch == "3":
+        year = input("조회할 년도를 입력해주세요.")
+        month = input("해당 년도의 조회할 월을 입력해주세요.")
+        day = input("조회할 일자를 입력해주세요.")
+        
+    else:
+        print("다시 입력해주세요.")
+        return
+    
+    #파일 찾기
+    search = []
+    for fname in files:
+        if not fname.endswith('.dat'):
+            continue
+        parts = fname[:-4].split('-')
+        if len(parts) != 4:
+            continue
+        y, m, d, uid = parts
+
+        if year and y != year:
+            continue
+        if month and m != month:
+            continue
+        if day and d != day:
+            continue
+        search.append(fname)
+        
+    if not search:
+        print("조건에 맞는 일정이 없습니다.")
+        return
+    
+    print("\n-----일정 목록-----")
+    for idx, fname in enumerate(search, 1):
+        data = load_file(fname)
+        print(f"   날짜 : {data['date']['year']}-{data['date']['month']}-{data['date']['day']}")
+        print(f"   고유 ID : {data['unique_id']}")
+        print("\n")
+    
+    #조회 창에서 삭제는 일정 삭제 함수 구현 후 이곳에 분기처리해 추가할 예정
+
+
+#일정 삭제
+def delete_schedule():
+    directory = "schedules"
+    if not os.path.exists(directory):
+        print("오류 : 일정이 존재하지 않습니다.")
+        return
+    
+    files = os.listdir(directory)
+    if not files:
+        print("오류 : 일정이 존재하지 않습니다.")
+        return
+    
+    year = input("삭제할 일정의 년도를 입력하세요")
+    month = input("해당 년도의 조회할 월을 입력해주세요.")
+    day = input("조회할 일자를 입력해주세요.")
+
+    search = []
+    for fname in files:
+        if not fname.endswith(".dat"):
+            continue
+        parts = fname[:-4].split('-')
+        if len(parts) != 4:
+            continue
+        y,m,d,uid = parts
+        if y == year and m == month and d == day:
+            search.append((fname, uid))
+        
+        if not search:
+            print("해당 날짜에 일정이 없습니다.")
+            return
+        
+    print(f"\n{year}-{month}-{day} 일정목록")
+    for i, (fname, uid) in enumerate(search, 1):
+        filepath = os.path.join(directory, fname)
+        with open(filepath, 'r', encoding = 'UTF-8') as f:
+            data = json.load(f)
+            print(f"{i}. 고유번호: {uid} - 내용: {data['schedule']}\n")
+
+    uid = input("삭제할 일정의 고유번호를 입력하세요: ")
+
+    target = None
+    for fname, file_uid in search:
+        if file_uid == uid:
+            target = fname
+            break
+
+    if not target:
+        print("해당 고유번호의 일정이 없습니다.")
+        return
+    
+    confirm = input(f"정말로 '{target}' 일정을 삭제하시겠습니까? (y/n) :")
+    if confirm.lower() != 'y':
+        print("삭제가 취소되었습니다.")
+        return
+    
+    filepath = os.path.join(directory, target)
+    os.remove(filepath)
+    print(f"'{target}' 일정이 삭제되었습니다.")
+
+
 #실행
 if __name__ == "__main__":
-    add_schedule()
+    delete_schedule()
+
+
+
