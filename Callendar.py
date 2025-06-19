4eaae940349bd433e7c0539f607b61be91e501f9
Jinhyuk <sungjinhyuk586@gmail.com>
2025-06-19 16:48:14 +0900

    캘린더 기반 기능 수정
    -시간, 분 단위 파싱 추가
    -동일 시간대 내 uid 순차적 증가

--- Callendar.py
+++ Callendar.py
@@ -4,20 +4,29 @@ from datetime import datetime
 import os
 
 #고유번호 생성(3자리)
-def generate_id():
-    return f"{random.randint(0, 999):03d}"
-
+def generate_id(y, m, d, h, mi):
+    # 마지막 파일 uid 불러오기
+    files = os.listdir("schedules")
+    if not files:return "001"
+    elif sorted(files)[-1].split('-')[0] != f"{y[2:]}{m:0>2}{d:0>2}{h:0>2}{mi:0>2}":return "001"
+    else:
+        print(sorted(files)[-1].split('-')[0], f"{y[2:]}{m:0>2}{d:0>2}{h:0>2}{mi:0>2}")
+        last_file = sorted(files)[-1]
+        last_id = int(last_file.split('-')[1].split('.')[0])
+        new_id = (last_id + 1) % 1000
+        return f"{new_id:03d}"
+    
 #연-월-일 파싱
 def parsing_date():
     now = datetime.now()
-    return str(now.year), str(now.month), str(now.day)
+    return str(now.year), str(now.month), str(now.day), str(now.hour), str(now.minute)
 
 #파일 저장 
-def save_file(year, month, day, uid):
+def save_file(year, month, day, h, m, uid):
     directory = "schedules"
     os.makedirs(directory, exist_ok=True)
 
-    filename = f"{year}-{month}-{day}-{uid}.dat"
+    filename = f"{year[2:]}{month:0>2}{day:0>2}{h:0>2}{m:0>2}-{uid}.dat"
     filepath = os.path.join(directory, filename)
 
     data = {
@@ -42,7 +51,7 @@ def load_file(filename):
 
 #실행
 if __name__ == "__main__":
-    uid = generate_id()
-    y, m, d = parsing_date()
-    filename = save_file(y, m, d, uid)
+    y, m, d, h, mi = parsing_date()
+    uid = generate_id(y, m, d, h, mi)
+    filename = save_file(y, m, d, h, mi, uid)
     print(f"일정이 저장되었습니다: {filename}")
