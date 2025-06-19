b04e62e501754842a9883e995b5ebce23843cf51
Jch03 <ken4098@naver.com>
2025-06-19 16:00:27 +0900

    일정추가 함수 구현 및 save_file 수정
    일정 추가 함수 구현에 따라, save_file 저장 파트에 "schedule" = content 추가

--- Callendar.py
+++ Callendar.py
@@ -12,31 +12,43 @@ def parsing_date():
     now = datetime.now()
     return str(now.year), str(now.month), str(now.day)
 
-#파일 저장
-def save_file(year, month, day, uid):
+#파일 저장 
+def save_file(year, month, day, uid, content=""):
+    directory = "schedules"
+    os.makedirs(directory, exist_ok=True)
+
     filename = f"{year}-{month}-{day}-{uid}.dat"
+    filepath = os.path.join(directory, filename)
+
     data = {
         "unique_id": uid,
         "date": {
             "year": year,
             "month": month,
             "day": day
-        }
+        },
+        "schedule": content
     }
-    with open(filename, 'w', encoding='utf-8') as f:
+    with open(filepath, 'w', encoding='utf-8') as f:
         json.dump(data, f, indent=4, ensure_ascii=False)
-    return filename
+    return filepath
 
 #저장된 파일 불러오기
 def load_file(filename):
-    if not os.path.exists(filename):
+    filepath = os.path.join("schedules", filename)
+    if not os.path.exists(filepath):
         return "오류 : 해당 일정이 존재하지 않습니다."
-    with open(filename, 'r', encoding='utf-8') as f:
+    with open(filepath, 'r', encoding='utf-8') as f:
         return json.load(f)
+
+def add_schedule():
+    content = input("일정 내용을 입력하세요 :")
+    uid = generate_id()
+    y,m,d = parsing_date()
+    filename = save_file(y, m, d, uid, content)
+    print(f"일정이 저장되었습니다: {filename}")
+    
 
 #실행
 if __name__ == "__main__":
-    uid = generate_id()
-    y, m, d = parsing_date()
-    filename = save_file(y, m, d, uid)
-    print(f"일정이 저장되었습니다.")
+    add_schedule()
