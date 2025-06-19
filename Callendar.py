d22d11ec260817f28ed36fad024134f7ae3a492f
Jinhyuk <sungjinhyuk586@gmail.com>
2025-06-19 17:56:55 +0900

    Edit Base Code
    - 입력 데이터 파싱기능 추가
    - 입력 없을 경우 디폴트 입력 추가

--- Callendar.py
+++ Callendar.py
@@ -7,25 +7,33 @@ def generate_id(y, m, d, h, mi):
     # 마지막 파일 uid 불러오기
     files = os.listdir("schedules")
     if not files:return "001"
-    elif sorted(files)[-1].split('-')[0] != f"{y[2:]}{m:0>2}{d:0>2}{h:0>2}{mi:0>2}":return "001"
+    elif sorted(files)[-1].split('-')[0] != f"{y}{m}{d}{h}{mi}":return "001"
     else:
-        print(sorted(files)[-1].split('-')[0], f"{y[2:]}{m:0>2}{d:0>2}{h:0>2}{mi:0>2}")
         last_file = sorted(files)[-1]
         last_id = int(last_file.split('-')[1].split('.')[0])
         new_id = (last_id + 1) % 1000
         return f"{new_id:03d}"
     
-#연-월-일 파싱
-def parsing_date():
-    now = datetime.now()
-    return str(now.year), str(now.month), str(now.day), str(now.hour), str(now.minute)
+#입력 데이터 파싱
+def parsing_input():
+    while True:
+        try:
+            s = input("일정을 입력하세요 (YYYY MM DD HH MM / Default=Now): ")
+            if s:
+                if datetime.strptime(s, "%Y %m %d %H %M"):
+                    s = list(s.split())
+                else:print("잘못된 날짜 형식입니다.")
+            else:s = list(datetime.now().strftime("%Y %m %d %H %M").split())
+            return s[0][2:], s[1], s[2], s[3], s[4]
+        except:
+            print("잘못된 입력 형식입니다.")  
 
 #파일 저장 
 def save_file(year, month, day, h, m, uid):
     directory = "schedules"
     os.makedirs(directory, exist_ok=True)
 
-    filename = f"{year[2:]}{month:0>2}{day:0>2}{h:0>2}{m:0>2}-{uid}.dat"
+    filename = f"{year}{month}{day}{h}{m}-{uid}.dat"
     filepath = os.path.join(directory, filename)
 
     data = {
@@ -52,7 +60,7 @@ def load_file(filename):
 
 #실행
 if __name__ == "__main__":
-    y, m, d, h, mi = parsing_date()
+    y, m, d, h, mi = parsing_input()
     uid = generate_id(y, m, d, h, mi)
     filename = save_file(y, m, d, h, mi, uid)
     print(f"일정이 저장되었습니다: {filename}")
